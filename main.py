1b0b07049c939c3fc3978a1724ad3e1085ec0cf4
Plamen Atanasov <spoder@gmail.com>
2022-02-06 20:44:39 +0200

    add restart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 GECKO_PATH = "E:\Development\geckodriver.exe"
 
@@ -14,3 +15,11 @@ body.send_keys(Keys.ARROW_UP)
 
 bot_box = driver.find_element_by_xpath('//*[@id="botStatus"]')
 bot_box.click()
+
+canvas = driver.find_element_by_xpath("/html/body/div[2]/div/canvas")
+
+game_on = True
+
+while game_on:
+    time.sleep(5)
+    canvas.click()
